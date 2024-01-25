77690ce3a60848ec7db079d6019cb17cd67b9ff2
syrup <88204504+VendelSz@users.noreply.github.com>
2024-01-25 23:54:09 +0100

    Add files via upload

--- Petrik Rage Game/scripts/Game Over.gd
+++ Petrik Rage Game/scripts/Game Over.gd
@@ -1,35 +1,56 @@
 extends Node2D
 
-var phoneguy_audio_stream_player : AudioStreamPlayer
-var stars_audio_stream_player : AudioStreamPlayer
-var yipe_audio_stream_player : AudioStreamPlayer
-var phoneguy_audio_stream : AudioStream
-var stars_audio_stream : AudioStream
-var yipe_audio_stream : AudioStream
+var aaahhh1_audio_stream_player : AudioStreamPlayer
+var aaahhh2_audio_stream_player : AudioStreamPlayer
+var augh1_audio_stream_player : AudioStreamPlayer
+var augh2_audio_stream_player : AudioStreamPlayer
+var nepityeregjel_audio_stream_player : AudioStreamPlayer
+var retkesfranciagecik_audio_stream_player : AudioStreamPlayer
+
+var aaahhh1_audio_stream : AudioStream
+var aaahhh2_audio_stream : AudioStream
+var augh1_audio_stream : AudioStream
+var augh2_audio_stream : AudioStream
+var nepityeregjel_audio_stream : AudioStream
+var retkesfranciagecik_audio_stream : AudioStream
 
 func _ready() -> void:
-	phoneguy_audio_stream_player = $phoneguy
-	stars_audio_stream_player = $stars
-	yipe_audio_stream_player = $yipe
+	aaahhh1_audio_stream_player = $aaahhh1
+	aaahhh2_audio_stream_player = $aaahhh2
+	augh1_audio_stream_player = $augh1
+	augh2_audio_stream_player = $augh2
+	nepityeregjel_audio_stream_player = $nepityeregjel
+	retkesfranciagecik_audio_stream_player = $retkesfranciagecik
 
-	phoneguy_audio_stream = preload("res://assets/deathsounds/fakeyou_rvc_f6r1f23nfgd1dykq5k2fx5yktybvr2cz.wav")
-	stars_audio_stream = preload("res://assets/deathsounds/Rah Band ~ Messages From The Stars 『Slowed & Reverb』.mp3")
-	yipe_audio_stream = preload("res://assets/deathsounds/Yipe.mp3")
+	aaahhh1_audio_stream = preload("res://assets/deathsounds/aaahhh1.mp3")
+	aaahhh2_audio_stream = preload("res://assets/deathsounds/aaahhh2.mp3")
+	augh1_audio_stream = preload("res://assets/deathsounds/augh1.mp3")
+	augh2_audio_stream = preload("res://assets/deathsounds/augh2.mp3")
+	nepityeregjel_audio_stream = preload("res://assets/deathsounds/ne pityeregjel.mp3")
+	retkesfranciagecik_audio_stream = preload("res://assets/deathsounds/retkes francia gecik.mp3")
 
-	phoneguy_audio_stream_player.stream = phoneguy_audio_stream
-	stars_audio_stream_player.stream = stars_audio_stream
-	yipe_audio_stream_player.stream = yipe_audio_stream
+	aaahhh1_audio_stream_player.stream = aaahhh1_audio_stream
+	aaahhh2_audio_stream_player.stream = aaahhh2_audio_stream
+	augh1_audio_stream_player.stream = augh1_audio_stream
+	augh2_audio_stream_player.stream = augh2_audio_stream
+	nepityeregjel_audio_stream_player.stream = nepityeregjel_audio_stream
+	retkesfranciagecik_audio_stream_player.stream = retkesfranciagecik_audio_stream
 	
 	randomize()
-	var random_number = randi() % 3 + 1
+	var random_number = randi() % 6 + 1
 	print("Random Number: ", random_number)
 	if random_number == 1:
-		phoneguy_audio_stream_player.play()
+		aaahhh1_audio_stream_player.play()
 	if random_number == 2:
-		stars_audio_stream_player.play()
+		aaahhh2_audio_stream_player.play()
 	if random_number == 3:
-		yipe_audio_stream_player.play()
-
+		augh1_audio_stream_player.play()
+	if random_number == 4:
+		augh2_audio_stream_player.play()
+	if random_number == 5:
+		nepityeregjel_audio_stream_player.play()
+	if random_number == 6:
+		retkesfranciagecik_audio_stream_player.play()
 
 
 func _on_Restart_pressed():
